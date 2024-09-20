1cab031cd2e11a35d7273af6e753051c5403a382
MaryAnn04 <maryannbarnabas4@gmail.com>
2024-09-20 09:01:05 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,20 +2,10 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
-#include <vector>
-#include <stdexcept>
 
 class StringCalculator {
 public:
-    int add(const std::string& numbers);
-
-private:
-    std::string extractDelimiter(std::string& numbers);
-    std::string replaceNewlines(const std::string& numbers, const std::string& delimiter);
-    std::vector<int> parseNumbers(const std::string& input, const std::string& delimiter);
-    void handleNegatives(const std::vector<int>& numbers);
-    int sumValidNumbers(const std::vector<int>& numbers);
-    bool isGreaterThanLimit(int number, int limit);
+    int add(const std::string& numbers); // Main function to add numbers
 };
 
 #endif // STRINGCALCULATOR_H
