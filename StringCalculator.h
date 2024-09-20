75151d889bdc8ffd493173a1a9140e4c65d0326f
MaryAnn04 <maryannbarnabas4@gmail.com>
2024-09-20 09:53:53 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -11,8 +11,8 @@ public:
 
 private:
     std::string extractDelimiter(std::string& numbers);
-    std::string replaceNewlines(const std::string& numbers, const std::string& delimiter);
-    std::vector<int> parseNumbers(const std::string& input, const std::string& delimiter);
+    std::string ReplaceNewlines(const std::string& numbers, const std::string& delimiter);
+    std::vector<int> ExtractingNumbers(const std::string& input, const std::string& delimiter);
     void handleNegatives(const std::vector<int>& numbers);
     int sumValidNumbers(const std::vector<int>& numbers);
     bool isGreaterThanLimit(int number, int limit);
