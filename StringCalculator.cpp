b6ad0cdd4c1b86a7e51ab9982884c3a9b064e98a
MaryAnn04 <maryannbarnabas4@gmail.com>
2024-09-20 09:15:33 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -4,41 +4,66 @@
 #include <vector>
 #include <string>
 
+// Main function to add numbers
 int StringCalculator::add(const std::string& numbers) {
     if (numbers.empty()) {
         return 0;
     }
 
-    std::string delimiter = ",";
     std::string input = numbers;
-    
-    // Custom delimiter case
+    std::string delimiter = getDelimiter(numbers, input);
+    std::vector<int> nums = extractNumbers(input, delimiter);
+    checkForNegatives(nums);
+
+    int sum = 0;
+    for (int num : nums) {
+        if (num <= 1000) {
+            sum += num;
+        }
+    }
+
+    return sum;
+}
+
+// Get the delimiter and modify the input if necessary
+std::string StringCalculator::getDelimiter(const std::string& numbers, std::string& input) {
     if (numbers.substr(0, 2) == "//") {
         size_t delimiterEnd = numbers.find("\n");
-        delimiter = numbers.substr(2, delimiterEnd - 2);
-        input = numbers.substr(delimiterEnd + 1);
+        std::string delimiter = numbers.substr(2, delimiterEnd - 2);
+        input = numbers.substr(delimiterEnd + 1);  // Skip the delimiter part
+        return delimiter;
     }
+    return ",";
+}
+
+// Split string into numbers and convert them
+std::vector<int> StringCalculator::extractNumbers(const std::string& input, const std::string& delimiter) {
+    std::vector<int> numbers;
+    std::string modifiedInput = input;
     
-    // Replace newline with the delimiter
-    for (char& ch : input) {
+    // Replace newlines with the delimiter
+    for (char& ch : modifiedInput) {
         if (ch == '\n') {
             ch = delimiter[0];
         }
     }
 
-    // Split input based on delimiter and sum the numbers
-    std::vector<int> nums;
-    std::stringstream ss(input);
+    std::stringstream ss(modifiedInput);
     std::string temp;
-    int sum = 0;
-    std::vector<int> negatives;
-    
+
     while (std::getline(ss, temp, delimiter[0])) {
-        int num = std::stoi(temp);
+        numbers.push_back(std::stoi(temp));
+    }
+
+    return numbers;
+}
+
+// Check if any negative numbers exist
+void StringCalculator::checkForNegatives(const std::vector<int>& numbers) {
+    std::vector<int> negatives;
+    for (int num : numbers) {
         if (num < 0) {
             negatives.push_back(num);
-        } else if (num <= 1000) {
-            sum += num;
         }
     }
 
@@ -49,6 +74,4 @@ int StringCalculator::add(const std::string& numbers) {
         }
         throw std::runtime_error(errorMsg);
     }
-
-    return sum;
 }
